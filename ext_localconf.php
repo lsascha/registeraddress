eade3352f49f2b3735bd9400a57c735587883911
Sascha Löffler <lsascha@gmail.com>
2018-04-02 19:50:23 +0200

    [TASK] Create hash on TCA entry creation

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,9 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+// Register Hook
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \AFM\Registeraddress\Hook\DataHandlerHook::class;
+
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
 	'AFM.' . $_EXTKEY,
 	'Registerform',
