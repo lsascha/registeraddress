c5ef7306a1ca07d4b1831048e2562b1164e57c80
eike.starkmann <eike.starkmann@undkonsorten.com>
2018-05-17 17:51:45 +0200

    [FEATURE]: add consent text
    This allows the extension to be dsgvo compatilbe together with
    registeraddress_logger

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,6 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['registeraddress_registerform'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('registeraddress_registerform',
+    'FILE:EXT:registeraddress/Configuration/FlexForms/flexform_registration.xml');
--- Configuration/TCA/Overrides/tx_registeraddress_domain_model_address.php
+++ Configuration/TCA/Overrides/tx_registeraddress_domain_model_address.php
@@ -11,8 +11,19 @@ $fields = array(
             'eval' => 'trim'
         ),
     ),
+    'consent' => array(
+        'exclude' => 0,
+        'label' => 'LLL:EXT:registeraddress/Resources/Private/Language/locallang_db.xml:tx_registeraddress_domain_model_address.consent',
+        'config' => array(
+            'type'     => 'text',
+            'readOnly' => 1,
+            'size'     => 30,
+            'eval' => 'trim'
+        ),
+    ),
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_address', $fields, TRUE);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_address', 'eigene_anrede', '', 'after:name');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('tt_address', 'eigene_anrede', '', 'after:name');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('tt_address', 'consent','','after:description');
