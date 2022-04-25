55285a4ce05539fee5fc79a1ca843eac5ec60f75
Karsten Nowak <nowak@undkonsorten.com>
2022-04-25 16:25:53 +0200

    [BUGFIX] Fix Reflection failed. error
    Refactor configurePlugin method
    
    Related: #21762

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,52 +1,52 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
 // Register Hook
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \AFM\Registeraddress\Hook\DataHandlerHook::class;
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'AFM.Registeraddress',
-	'Registerform',
-	array(
-		'Address' => 'new, create, approve, edit, update, delete, information',
+    'Registeraddress',
+    'Registerform',
+    array(
+        \AFM\Registeraddress\Controller\AddressController::class => 'new, create, approve, edit, update, delete, information',
 
-	),
-	// non-cacheable actions
-	array(
-		'Address' => 'new, create, approve, edit, update, delete, information',
+    ),
+    // non-cacheable actions
+    array(
+        \AFM\Registeraddress\Controller\AddressController::class => 'new, create, approve, edit, update, delete, information',
 
-	)
+    )
 );
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'AFM.Registeraddress',
-	'RegisterformRedirect',
-	array(
-		'Address' => 'new',
+    'Registeraddress',
+    'RegisterformRedirect',
+    array(
+        \AFM\Registeraddress\Controller\AddressController::class => 'new',
 
-	),
-	// non-cacheable actions
-	array(
-		'Address' => 'new',
+    ),
+    // non-cacheable actions
+    array(
+        \AFM\Registeraddress\Controller\AddressController::class => 'new',
 
-	)
+    )
 );
 
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'AFM.Registeraddress',
-	'RegisterformUnsubscribe',
-	array(
-		'Address' => 'unsubscribeForm',
+    'Registeraddress',
+    'RegisterformUnsubscribe',
+    array(
+        \AFM\Registeraddress\Controller\AddressController::class => 'unsubscribeForm',
 
-	),
-	// non-cacheable actions
-	array(
-		'Address' => 'unsubscribeForm',
+    ),
+    // non-cacheable actions
+    array(
+        \AFM\Registeraddress\Controller\AddressController::class => 'unsubscribeForm',
 
-	)
+    )
 );
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\AFM\Registeraddress\Task\DeleteHiddenRegistrationsTask::class] = [
