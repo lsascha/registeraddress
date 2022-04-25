21fe14e00ffb75f1d91eb48daa960be005fd9882
Karsten Nowak <nowak@undkonsorten.com>
2022-04-25 15:33:58 +0200

    [WIP][TASK] Fix tt_address dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '10.4.0 - 11.5.99',
-			'tt_address' => '5.2.0-5.99.99',
+			'tt_address' => '5.2.0-6.99.99',
 		),
 		'conflicts' => array(
 		),
