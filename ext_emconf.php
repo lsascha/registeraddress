d88255e920c527442a030c307e9c154281ba6d61
Karsten Nowak <nowak@undkonsorten.com>
2022-04-25 15:29:41 +0200

    [WIP][TASK] Start develop for TYPO3 11.5
    Related:

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.0.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '10.4.0 - 10.4.99',
+			'typo3' => '10.4.0 - 11.5.99',
 			'tt_address' => '5.2.0-5.99.99',
 		),
 		'conflicts' => array(
