9785e4612a236156515b1c4ad9f72e097f581959
Eike Starkmann <eike.starkmann@posteo.de>
2023-05-03 09:50:19 +0200

    [TASK] add dependencies for TYPO3 12.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '4.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '11.5.0 - 11.5.99',
+			'typo3' => '11.5.0 - 12.9.99',
 			'tt_address' => '5.2.0-7.99.99',
 		),
 		'conflicts' => array(
